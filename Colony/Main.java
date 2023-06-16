6595658869d18c857b6632796a0eb90c43510d45
jorge-machado <73745117+jorge-machado@users.noreply.github.com>
2023-06-16 12:08:37 +0100

    Add files via upload

--- Colony/Main.java
+++ Colony/Main.java
@@ -1,15 +1,13 @@
 package Colony;
 
-// import java.util.*;
-
 public class Main {
     static Discrete_Stochastic_Simulation DSS;
 
     public static void main(String[] args) {
     	// test:
     	// String[] args_main = { "-r", "5", "1", "1", "4", "5", "6", "7", "8", "9", "1", "10" };
-    	String[] args_main = { "-f", "SIM/input5.txt"};
-        DSS = new Discrete_Stochastic_Simulation(args_main);
+    	// String[] args_main = { "-f", "SIM/input5.txt"};
+        DSS = new Discrete_Stochastic_Simulation(args);
         DSS.Simulation();
     }
 }
