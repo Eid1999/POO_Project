79420236a6e69fccb305014b71a2a2dce48dfed6
andrebarreiros99 <80112551+andrebarreiros99@users.noreply.github.com>
2023-06-16 12:19:21 +0100

    edgeInterface

--- Colony/EdgeInterface.java
+++ Colony/EdgeInterface.java
@@ -4,4 +4,7 @@ public interface EdgeInterface<V> {
     V getDestination();
     double getWeight();
     V getSource();
+    public double getPheromone();
+    public void setPheromone(double pheromone);
 }
+
