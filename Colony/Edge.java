29efcef983b286500c832308d8810368520d00cc
Eid1999 <pedrohenrique_eid@hotmail.com>
2023-06-16 13:58:52 +0100

    Javadocs

--- Colony/Edge.java
+++ Colony/Edge.java
@@ -1,11 +1,20 @@
 package Colony;
-
-public class Edge{
+/**
+ * Represents an edge in a graph, connecting a source node to a destination node.
+ */
+public class Edge {
     private String source;
     private String destination;
     private double weight;
     private double pheromone;
 
+    /**
+     * Constructs an edge with the given source, destination, and weight.
+     *
+     * @param source      The source node of the edge.
+     * @param destination The destination node of the edge.
+     * @param weight      The weight of the edge.
+     */
     public Edge(String source, String destination, double weight) {
         this.source = source;
         this.destination = destination;
@@ -13,27 +22,53 @@ public class Edge{
         this.pheromone = 0;
     }
 
+    /**
+     * Gets the destination node of the edge.
+     *
+     * @return The destination node.
+     */
     public String getDestination() {
         return destination;
     }
 
+    /**
+     * Gets the weight of the edge.
+     *
+     * @return The weight of the edge.
+     */
     public double getWeight() {
         return weight;
     }
 
+    /**
+     * Gets the source node of the edge.
+     *
+     * @return The source node.
+     */
     public String getSource() {
         return source;
     }
 
-    public double getPheromone(){
+    /**
+     * Gets the pheromone level of the edge.
+     *
+     * @return The pheromone level.
+     */
+    public double getPheromone() {
         return pheromone;
     }
-    public void setPheromone(double pheromone){
+
+    /**
+     * Sets the pheromone level of the edge.
+     *
+     * @param pheromone The pheromone level to set.
+     */
+    public void setPheromone(double pheromone) {
         this.pheromone = pheromone;
     }
+
     @Override
     public String toString() {
         return source + " -> " + destination + " (Weight: " + weight + ", Pheromone: " + pheromone + ")";
     }
-
 }
--- Colony/Main.java
+++ Colony/Main.java
@@ -6,14 +6,9 @@ public class Main {
     public static void main(String[] args) {
     	// test:
     	// String[] args_main = { "-r", "5", "1", "1", "4", "5", "6", "7", "8", "9", "1", "10" };
-<<<<<<< Updated upstream
-    	// String[] args_main = { "-f", "SIM/input8.txt"};
-        // DSS = new Discrete_Stochastic_Simulation(args_main);
-    	DSS = new Discrete_Stochastic_Simulation(args);
-=======
-    	String[] args_main = { "-f", "SIM/input1.txt"};
+    	String[] args_main = { "-f", "SIM/input7.txt"};
         DSS = new Discrete_Stochastic_Simulation(args_main);
->>>>>>> Stashed changes
+
         DSS.Simulation();
     }
 }
