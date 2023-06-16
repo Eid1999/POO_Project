1c5acecbf38e51d79826cad6f0959596be033fc0
jorge-machado <73745117+jorge-machado@users.noreply.github.com>
2023-06-16 13:17:31 +0100

    comment test input on main

--- Colony/Main.java
+++ Colony/Main.java
@@ -6,8 +6,9 @@ public class Main {
     public static void main(String[] args) {
     	// test:
     	// String[] args_main = { "-r", "5", "1", "1", "4", "5", "6", "7", "8", "9", "1", "10" };
-    	String[] args_main = { "-f", "SIM/input5.txt"};
-        DSS = new Discrete_Stochastic_Simulation(args_main);
+    	// String[] args_main = { "-f", "SIM/input8.txt"};
+        // DSS = new Discrete_Stochastic_Simulation(args_main);
+    	DSS = new Discrete_Stochastic_Simulation(args);
         DSS.Simulation();
     }
 }
